0c61868565411075b0af322ffd987f0aaecb89e5
Ahmad Abu gharbia <aag@Ahmads-MacBook-Pro.local>
2023-07-08 07:57:36 -0700

    Update run_task

--- data.tf
+++ data.tf
@@ -4,10 +4,10 @@ data "aws_region" "current" {}
 
 # Locals for ease of use
 
-locals {
-    account_id = data.aws_caller_identity.current.account_id
-}
+#locals {
+#    account_id = data.aws_caller_identity.current.account_id
+#}
 
-locals {
-    region = data.aws_region.current.name
-}
+#locals {
+#    region = data.aws_region.current.name
+#}
